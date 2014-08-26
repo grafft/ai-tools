b3b17a1e4bf6dbb006e6cc4560bdc58881925f81
grafft <sanekpanov@gmail.com>
2014-08-26 10:15:16 +0400

    [+] Norris algorithm

--- ai-causal/src/main/java/ru/isa/ai/causal/GAAQJSM.java
+++ ai-causal/src/main/java/ru/isa/ai/causal/GAAQJSM.java
@@ -70,15 +70,15 @@ public class GAAQJSM {
                     };
                     loader.setSource(new File(dataFile));
                     loader.setFieldSeparator("\t");
-                    loader.setNominalAttributes("29");
-                    loader.setNominalLabelSpecs(new String[]{"29:1,2,3"});
-                    ConverterUtils.DataSource trainSource = new ConverterUtils.DataSource(loader);
-                    data = trainSource.getDataSet(28);
+//                    loader.setNominalAttributes("29");
+//                    loader.setNominalLabelSpecs(new String[]{"29:1,2,3"});
+//                    ConverterUtils.DataSource trainSource = new ConverterUtils.DataSource(loader);
+//                    data = trainSource.getDataSet(28);
 
-                    //loader.setNominalAttributes("1-3");
-                    //loader.setNominalLabelSpecs(new String[]{"1:1,2,3", "2:1,2", "3:1,2"});
-                    //ConverterUtils.DataSource trainSource = new ConverterUtils.DataSource(loader);
-                    //data = trainSource.getDataSet(0);
+                    loader.setNominalAttributes("1-3");
+                    loader.setNominalLabelSpecs(new String[]{"1:1,2,3", "2:1,2", "3:1,2"});
+                    ConverterUtils.DataSource trainSource = new ConverterUtils.DataSource(loader);
+                    data = trainSource.getDataSet(0);
                 } else {
                     throw new AQClassifierException("Not supported file extension: " + dataFile);
                 }
